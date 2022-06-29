8070c032f4cb10b6d82b4a0b104a0e71b1e0f0f7
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-06-29 22:07:07 +0600

    add view page of list students and create dbclass

---
+++ Learn_AspDotNetMVC/App_Data/DBClass.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web.Razor.Parser;
+using Learn_AspDotNetMVC.Models.TutorialsTeacher;
+
+namespace Learn_AspDotNetMVC.App_Data
+{
+    public static class DBClass
+    {
+
+        public static IList<Student> studentList = new List<Student>
+        {
+            new Student() {Id = 1, Name = "Potato MIa", Age = "23"},
+            new Student() {Id = 2, Name = "Alu MIa", Age = "23"},
+            new Student() {Id = 3, Name = "Khazu MIa", Age = "23"},
+            new Student() {Id = 4, Name = "Kobra MIa", Age = "23"},
+            new Student() {Id = 5, Name = "Huzu MIa", Age = "23"},
+            new Student() {Id = 6, Name = "Abal MIa", Age = "23"},
+        };
+    }
+}
--- Learn_AspDotNetMVC/Controllers/TutorialsTeacher/StudentController.cs
+++ Learn_AspDotNetMVC/Controllers/TutorialsTeacher/StudentController.cs
@@ -3,30 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Learn_AspDotNetMVC.App_Data;
 using Learn_AspDotNetMVC.Models.TutorialsTeacher;
 
 namespace Learn_AspDotNetMVC.Controllers.TutorialsTeacher
 {
     public class StudentController : Controller
     {
-        // GET: Student
-        public string Index()
+        public StudentController()
         {
-            return "This is Index action method of StudentController";
+            
+        }
+        
+        //get students
+        public ActionResult Index()
+        {
+            return View(DBClass.studentList);
         }
 
-        [HttpPost]
-        public ActionResult Edit(Student std)
-        {
-            // update data to database for student
-            return RedirectToAction("Index");
-        }
-
-        [HttpDelete]
-        public ActionResult Delete(int id)
-        {
-            // delete specifiq student based on ID
-            return RedirectToAction("Index");
-        }
     }
 }

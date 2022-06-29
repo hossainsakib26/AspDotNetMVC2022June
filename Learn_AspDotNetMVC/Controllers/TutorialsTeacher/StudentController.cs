43cffa14e83044410a18b6a25b1ce9a5abc7beee
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-06-29 09:49:42 +0600

    Add model for student and link it to student controller

--- Learn_AspDotNetMVC/Controllers/TutorialsTeacher/StudentController.cs
+++ Learn_AspDotNetMVC/Controllers/TutorialsTeacher/StudentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Learn_AspDotNetMVC.Models.TutorialsTeacher;
 
 namespace Learn_AspDotNetMVC.Controllers.TutorialsTeacher
 {
@@ -12,6 +13,20 @@ namespace Learn_AspDotNetMVC.Controllers.TutorialsTeacher
         public string Index()
         {
             return "This is Index action method of StudentController";
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            // update data to database for student
+            return RedirectToAction("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            // delete specifiq student based on ID
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Learn_AspDotNetMVC/Models/TutorialsTeacher/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learn_AspDotNetMVC.Models.TutorialsTeacher
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+    }
+}

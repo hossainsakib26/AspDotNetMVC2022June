a34fc620365dc0cc6f196dcbc3ba7d5e618e7109
Sazzad Hossain <hossain.sakib26@hotmail.com>
2022-07-23 21:09:22 +0600

    Gender enum added

---
+++ Learn_AspDotNetMVC/Models/Enum/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Learn_AspDotNetMVC.Models.Enum
+{
+    public enum Gender
+    {
+        Male,
+        Female,
+        Transgender
+    }
+}
